7a62017294fb8f863ca9b63de20d30cff875c48c
Hongyu <hy2504@columbia.edu>
2017-02-10 22:41:00 -0500

    finish the Card deck

---
+++ src/Deck.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Deck {
+  private final List<Card> cards;
+  private int indexOfDeck;
+  public Deck() {
+    cards = new ArrayList<Card>();
+    for (int i = 1; i <= 13; i++) {
+      for (Suit s : Suit.values()) {
+        cards.add(new Card(i, s));
+      }
+    }
+  }
+  
+  //before start every round, we should shuffle the deck to a random order. We can use an array to record the order
+  public void shuffle() {
+    for (int i = cards.size() - 1; i > 0; i--) {
+      int index = (int) Math.random() * (i + 1);
+      Card c1 = cards.get(index);
+      Card c2 = cards.get(i);
+      cards.set(index, c2);
+      cards.set(i, c1);
+    }
+  }
+  
+  private int remainingCard() {
+    return cards.size() - indexOfDeck;
+  }
+  
+  public Card[] dealHand(int number) {
+    if (number > remainingCard()) {
+      return null;
+    }
+    Card[] C = new Card[number];
+    for (int i = 0; i < number; i++) {
+      C[i] = dealCard();
+    }
+    return C;
+  }
+  
+  public Card dealCard() {
+    if (remainingCard() <= 0) {
+      return null;
+    }
+    return cards.get(indexOfDeck++);
+  }
+  
+}
